e0ed13ac44811c808575862363d60af5ce2d6a30
Orestis Gkorgkas <og@bibsys.no>
2019-03-05 13:57:22 +0100

    function for adding multiple keys in Jackson Json

--- src/main/java/utils/JsonUtils.java
+++ src/main/java/utils/JsonUtils.java
@@ -2,11 +2,15 @@ package utils;
 
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonParser.Feature;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public final class JsonUtils {
 
@@ -24,11 +28,52 @@ public final class JsonUtils {
     }
 
 
+    public static String write(Object item) throws JsonProcessingException {
+        return writer.writeValueAsString(item);
+    }
+
     public static ObjectNode newObjectNode(){
        return  mapper.createObjectNode();
     }
 
     public static ArrayNode newArrayNode(){
         return mapper.createArrayNode();
+    }
+
+
+    public static ObjectNode putKeyInNode(String key,String value,ObjectNode  root) throws IOException {
+        List<String> keys = Arrays.asList(key.split("\\."));
+        ObjectNode currentNode=root;
+        for(int i=0; i<keys.size()-1; i++){
+            String currentKey=keys.get(i);
+            if(!hasKey(root, keys.subList(0,i+1))) {
+                currentNode.set(currentKey, JsonUtils.newObjectNode());
+            }
+            currentNode = (ObjectNode) currentNode.get(currentKey);
+        }
+
+        String currentKey=keys.get(keys.size()-1);
+        currentNode.put(currentKey,value);
+        return root;
+    }
+
+
+
+    private static boolean hasKey(ObjectNode root,List<String>keys) throws IOException {
+        ObjectNode currentNode=root;
+
+        for(int i=0;i<keys.size();i++){
+            String currentKey=keys.get(i);
+            if(currentNode.has(currentKey)){
+                currentNode=(ObjectNode)currentNode.get(currentKey);
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+
+
+
     }
 }
---
+++ src/test/java/utils/JsonUtilsTest.java
@@ -0,0 +1,63 @@
+package utils;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+
+class JsonUtilsTest {
+
+    private static final String KEY_DELIMITER = ".";
+
+    @Test
+    void putKeyInNodeShouldPutASingleKeyInTheObjectNode() throws IOException {
+        ObjectNode root = JsonUtils.newObjectNode();
+        String key = "key";
+        String value = "value";
+        root = JsonUtils.putKeyInNode(key, value, root);
+        String actualValue = root.get(key).asText();
+        assertThat(actualValue, is(equalTo(value)));
+    }
+
+    @Test
+    void putMultipleKeyInNodeShouldPutAMutlipleeKeyInTheObjectNode() throws IOException {
+        ObjectNode root = JsonUtils.newObjectNode();
+        String key1 = "key1";
+        String key2 = "key2";
+        String key3 = "key3";
+        String key=String.join(".",key1,key2,key3);
+        String value = "value";
+        root = JsonUtils.putKeyInNode(key, value, root);
+        String actualValue = root.get(key1).get(key2).get(key3).asText();
+        assertThat(actualValue, is(equalTo(value)));
+    }
+
+
+    @Test
+    void putMKeyInNodeShouldMaintainCommonParentsInKeysInTheObjectNode() throws IOException {
+        ObjectNode root = JsonUtils.newObjectNode();
+        String key1 = "key1";
+        String key2 = "key2";
+        String key3 = "key3";
+        String key4 = "key4";
+
+        String keyA=String.join(KEY_DELIMITER,key1,key2,key3);
+        String keyB=String.join(".",key1,key2,key4);
+
+        String valueA = "valueA";
+        String valueB = "valueB";
+        root = JsonUtils.putKeyInNode(keyA, valueA, root);
+        root = JsonUtils.putKeyInNode(keyB, valueB, root);
+        String actualValueA = root.get(key1).get(key2).get(key3).asText();
+        assertThat(actualValueA, is(equalTo(valueA)));
+        String actualValueB = root.get(key1).get(key2).get(key4).asText();
+        assertThat(actualValueB, is(equalTo(valueB)));
+    }
+
+
+
+
+}
