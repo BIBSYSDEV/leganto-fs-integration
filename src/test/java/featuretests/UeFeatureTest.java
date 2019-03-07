8c79a220160700a75a99a005ccbc25e2ec58210b
Orestis Gkorgkas <og@bibsys.no>
2019-03-07 12:41:01 +0100

    LAI-2: UeFeatureTests sketch

---
+++ src/test/java/featuretests/UeFeatureTest.java
@@ -0,0 +1,25 @@
+package featuretests;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+
+public class UeFeatureTest {
+
+    @Given("there is a request to \\/undervisning\\/UE_ID")
+    public void there_is_a_request_to_undervisning_UE_ID() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Given("the response from \\/undervising\\/UE_ID has a field with name {string} with value {string}")
+    public void the_response_from_undervising_UE_ID_has_a_field_with_name_with_value(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("a new UE entry is generated")
+    public void a_new_UE_entry_is_generated() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+}
