723ed4e39553e2cd09d8b0ab38e15a7756e8abc1
Orestis Gkorgkas <og@bibsys.no>
2019-03-11 15:29:15 +0100

    LAI-2: JsonUtils traversal and parsing

--- src/main/java/utils/JsonUtils.java
+++ src/main/java/utils/JsonUtils.java
@@ -3,8 +3,8 @@ package utils;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonParser.Feature;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectReader;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -13,10 +13,12 @@ import java.util.List;
 
 public final class JsonUtils {
 
-    private static final String UNSUPPORTED_CLASS_EXCEPTION = "Class %s is not yet support it. Add it in JsonTools";
+    protected static final String JSON_FIELDS_DELIMITER = ".";
+    private static final String UNSUPPORTED_CLASS_EXCEPTION = "Class %s is not yet supported. Add it in JsonTools";
+    private static final int EXCLUDE_LAST_KEY = 1;
+    private static final String JSON_FIELDS_SPlIT_REGEX = "\\.";
     public static ObjectMapper mapper = jsonParser();
-    public static ObjectWriter writer = mapper.writer();
-    public static ObjectReader reader = mapper.reader();
+    private static ObjectWriter writer = mapper.writer();
 
     private JsonUtils() {
     }
@@ -40,8 +42,8 @@ public final class JsonUtils {
         return mapper.createArrayNode();
     }
 
-    public static <T> ObjectNode putKeyInNode(String key, T value, ObjectNode root) {
-        List<String> keys = Arrays.asList(key.split("\\."));
+    public static <T> ObjectNode putKeyInNode(ObjectNode root, String key, T value) {
+        List<String> keys = Arrays.asList(key.split(JSON_FIELDS_SPlIT_REGEX));
         ObjectNode currentNode = root;
         for (int i = 0; i < keys.size() - 1; i++) {
             String currentKey = keys.get(i);
@@ -57,27 +59,77 @@ public final class JsonUtils {
         return root;
     }
 
-    private static <T> void putKey(String key, T value, ObjectNode node) {
+    private static <T> ObjectNode putKey(String key, T value, ObjectNode node) {
 
         if (value instanceof String) {
             node.put(key, (String) value);
         } else if (value instanceof Integer) {
             node.put(key, (Integer) value);
+        } else if (value instanceof Boolean) {
+            node.put(key, (Boolean) value);
+        } else if (value instanceof ArrayNode) {
+            node.put(key, (ArrayNode) value);
         } else {
             throw new IllegalArgumentException(String.format(UNSUPPORTED_CLASS_EXCEPTION, value.getClass().toString()));
         }
+        return node;
+    }
+
+    public static boolean hasKey(ObjectNode root, String key) {
+        List<String> keys = Arrays.asList(key.split(JSON_FIELDS_SPlIT_REGEX));
+        return hasKey(root, keys);
     }
 
     private static boolean hasKey(ObjectNode root, List<String> keys) {
         ObjectNode currentNode = root;
 
-        for (String currentKey : keys) {
+        for (int i = 0; i < keys.size() - EXCLUDE_LAST_KEY; i++) {
+            String currentKey = keys.get(i);
             if (currentNode.has(currentKey)) {
                 currentNode = (ObjectNode) currentNode.get(currentKey);
             } else {
                 return false;
             }
         }
-        return true;
+        String lastKey = keys.get(keys.size() - 1);
+        return currentNode.has(lastKey);
+    }
+
+    public static JsonNode traverseCompositeKey(ObjectNode root, String key) {
+        List<String> keys = Arrays.asList(key.split(JSON_FIELDS_SPlIT_REGEX));
+        return traverseCompositeKey(root, keys);
+    }
+
+    private static JsonNode traverseCompositeKey(ObjectNode root, List<String> keys) {
+        JsonNode current = root;
+        for (String currentKey : keys) {
+            current = current.get(currentKey);
+        }
+        return current;
+    }
+
+    public static ObjectNode removeKeyFromNode(ObjectNode root, String key) {
+        if (hasKey(root, key)) {
+            List<String> keys = Arrays.asList(key.split(JSON_FIELDS_SPlIT_REGEX));
+            List<String> secondToLastKey = keys.subList(0, keys.size() - EXCLUDE_LAST_KEY);
+            ObjectNode parent = (ObjectNode) traverseCompositeKey(root, secondToLastKey);
+            String lastKey = keys.get(keys.size() - 1);
+            parent.remove(lastKey);
+            System.out.print(parent);
+        }
+        return root;
+    }
+
+    public static ObjectNode putElementArrayInNode(ObjectNode root, String key, List<ObjectNode> arrayValues) {
+        ArrayNode arrayNode = newArrayNode();
+        putKeyInNode(root, key, arrayNode);
+        arrayValues.forEach(arrayNode::add);
+        return root;
+    }
+
+    public static <T> T readValue(ObjectNode root, Class<T> clazz) throws JsonProcessingException {
+        return mapper.treeToValue(root, clazz);
+
+
     }
 }
--- src/test/java/utils/JsonUtilsTest.java
+++ src/test/java/utils/JsonUtilsTest.java
@@ -3,41 +3,54 @@ package utils;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
+import static utils.JsonUtils.hasKey;
+import static utils.JsonUtils.newObjectNode;
+import static utils.JsonUtils.putKeyInNode;
 
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class JsonUtilsTest {
 
     private static final String KEY_DELIMITER = ".";
+    private static final String KEY_1 = "key1";
+    private static final String KEY_2 = "key2";
+    private static final String KEY_3A = "key3";
+    private static final String KEY_A = String.join(".", KEY_1, KEY_2, KEY_3A);
+    private static final String KEY_3B = "key4";
+    private static final String KEY_B = String.join(".", KEY_1, KEY_2, KEY_3B);
+    private static final String KEY_A_VALUE = "valueA";
+    private static final String KEY_B_VALUE = "valueB";
 
     @Test
-    void putKeyInNodeShouldPutASingleKeyInTheObjectNode() throws IOException {
-        ObjectNode root = JsonUtils.newObjectNode();
+    void putKeyInNodeShouldPutASingleKeyInTheObjectNode() {
+        ObjectNode root = newObjectNode();
         String key = "key";
         String value = "value";
-        root = JsonUtils.putKeyInNode(key, value, root);
+        putKeyInNode(root, key, value);
         String actualValue = root.get(key).asText();
         assertThat(actualValue, is(equalTo(value)));
     }
 
     @Test
-    void putMultipleKeyInNodeShouldPutAMutlipleeKeyInTheObjectNode() throws IOException {
-        ObjectNode root = JsonUtils.newObjectNode();
+    void putMultipleKeyInNodeShouldPutAMutlipleeKeyInTheObjectNode() {
+        ObjectNode root = newObjectNode();
         String key1 = "key1";
         String key2 = "key2";
         String key3 = "key3";
         String key = String.join(".", key1, key2, key3);
         String value = "value";
-        root = JsonUtils.putKeyInNode(key, value, root);
+        putKeyInNode(root, key, value);
         String actualValue = root.get(key1).get(key2).get(key3).asText();
         assertThat(actualValue, is(equalTo(value)));
     }
 
     @Test
-    void putMKeyInNodeShouldMaintainCommonParentsInKeysInTheObjectNode() throws IOException {
-        ObjectNode root = JsonUtils.newObjectNode();
+    void putMKeyInNodeShouldMaintainCommonParentsInKeysInTheObjectNode() {
+        ObjectNode root = newObjectNode();
         String key1 = "key1";
         String key2 = "key2";
         String key3 = "key3";
@@ -48,11 +61,56 @@ class JsonUtilsTest {
 
         String valueA = "valueA";
         String valueB = "valueB";
-        root = JsonUtils.putKeyInNode(keyA, valueA, root);
-        root = JsonUtils.putKeyInNode(keyB, valueB, root);
+        putKeyInNode(root, keyA, valueA);
+        putKeyInNode(root, keyB, valueB);
         String actualValueA = root.get(key1).get(key2).get(key3).asText();
         assertThat(actualValueA, is(equalTo(valueA)));
         String actualValueB = root.get(key1).get(key2).get(key4).asText();
         assertThat(actualValueB, is(equalTo(valueB)));
+    }
+
+    @Test
+    void removeKeyFromNodeShouldDoNothingIfKeyDoesNotExist() {
+        ObjectNode root = newObjectNode();
+        putKeyInNode(root, KEY_A, KEY_A_VALUE);
+        JsonUtils.removeKeyFromNode(root, KEY_B);
+        assertThat(JsonUtils.hasKey(root, KEY_A), is(equalTo(true)));
+        assertThat(JsonUtils.hasKey(root, KEY_B), is(equalTo(false)));
+    }
+
+    @Test
+    void removeKeyFromNodeShouldDoRemoveKeyIfKeyExists() {
+        ObjectNode root = newObjectNode();
+        putKeyInNode(root, KEY_A, KEY_A_VALUE);
+        putKeyInNode(root, KEY_B, KEY_B_VALUE);
+        JsonUtils.removeKeyFromNode(root, KEY_B);
+        assertThat(JsonUtils.hasKey(root, KEY_A), is(equalTo(true)));
+        assertThat(JsonUtils.hasKey(root, KEY_B), is(equalTo(false)));
+    }
+
+    @Test
+    void putElementArrayInNodeShouldCreateAnArrayNodeForTheKey() {
+        ObjectNode root = newObjectNode();
+        ObjectNode node1 = newObjectNode().put(KEY_3A, KEY_A_VALUE);
+        ObjectNode node2 = newObjectNode().put(KEY_3B, KEY_B_VALUE);
+        String key = String.join(JsonUtils.JSON_FIELDS_DELIMITER, KEY_1, KEY_2);
+        List<ObjectNode> nodeList = new ArrayList<>();
+        nodeList.add(node1);
+        nodeList.add(node2);
+        JsonUtils.putElementArrayInNode(root, key, nodeList);
+
+        assertThat(hasKey(root, key), is(equalTo(true)));
+
+        ArrayNode node = (ArrayNode) JsonUtils.traverseCompositeKey(root, key);
+        assertThat(node.size(), is(equalTo(nodeList.size())));
+
+        assertAllElementsInArrayElementAreContainedInInputLIst(nodeList, node);
+    }
+
+    private void assertAllElementsInArrayElementAreContainedInInputLIst(List<ObjectNode> nodeList, ArrayNode node) {
+        for (int i = 0; i < node.size(); i++) {
+            ObjectNode arrayElement = (ObjectNode) node.get(i);
+            assertThat(nodeList.contains(arrayElement), is(equalTo(true)));
+        }
     }
 }
